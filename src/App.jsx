9937f61f62d288e4888e8d1c9e1e1ddd3aeb23dc
KillerMeritus <senv9098@gmail.com>
2026-04-06 01:15:55 +0530

    feat: integrate app routing and global styles

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import { useEffect } from 'react'
+import { getProducts } from './services/api'
+import { Routes,Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Products from './pages/Products'
+import ProductDetails from './pages/ProductDetails'
+import Cart from './pages/Cart'
+import Wishlist from './pages/Wishlist'
+import Checkout from './pages/Checkout'
+
+function App() {
+  useEffect(() => {
+    getProducts().then(res => console.log(res.data))
+  }, [])
+
+  return (
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/products" element={<Products/>}/>
+      <Route path="/products/:id" element={<ProductDetails/>}/>
+      <Route path="/cart" element={<Cart/>}/>
+      <Route path="/wishlist" element={<Wishlist/>}/>
+      <Route path="/checkout" element={<Checkout/>}/>
+    </Routes>
+
+    
+  )
+}
+
+export default App
+
